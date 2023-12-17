1093aef537cbdf1cbbe04f2720f8d2f317c60bf8
subhashishjungshah <subhashishjungshah@gmail.com>
2023-12-17 17:58:30 +0545

    interacting with matic token

--- contract.js
+++ contract.js
@@ -1,3 +1,23 @@
 const web3 = require("./client");
 
 const maticABI = require("./abi");
+
+const maticAddress = "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0";
+
+// Talking to a contract in ethereum mainnet
+async function init() {
+  const contract = new web3.eth.Contract(maticABI, maticAddress);
+  //   console.log(contract.methods);
+  const tokenName = await contract.methods.name().call();
+  const tokenSymbol = await contract.methods.symbol().call();
+  const totalSupply = await contract.methods.totalSupply().call();
+  const balanceOf = await contract.methods
+    .balanceOf("0xCDCFaB82b9567e36394Cc72460459C19A260Bb67")
+    .call();
+  console.log(tokenName);
+  console.log(tokenSymbol);
+  //   console.log(totalSupply);
+  console.log(balanceOf);
+}
+
+init();
